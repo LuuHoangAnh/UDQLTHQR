49d93e28e37654d88d1614ee4ecbd08414402089
LuuHoangAnh <luuhoanganh3122003@gmail.com>
2024-10-18 00:09:00 +0700

    fix SellActivity

--- app/src/main/java/com/example/test/SellActivity.java
+++ app/src/main/java/com/example/test/SellActivity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -26,6 +27,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class SellActivity extends AppCompatActivity {
 //    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://udqlthqr-default-rtdb.firebaseio.com/");
@@ -68,10 +70,16 @@ public class SellActivity extends AppCompatActivity {
 
     }
 
+    Random random = new Random();
+    int number = random.nextInt(999999) + 1;
 
-     //Phương thức tải dữ liệu sản phẩm từ Firebase và cập nhật vào ListView
+    // Định dạng số để đảm bảo nó có 6 chữ số
+    String billCode = String.format("%06d", number);
+
+
+    //Phương thức tải dữ liệu sản phẩm từ Firebase và cập nhật vào ListView
     private void loadProductsFromFirebase() {
-        productsRepository.addValueEventListener(new ValueEventListener() {
+        billRepository.child(billCode).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 productList.clear(); // Xóa dữ liệu cũ
@@ -115,40 +123,92 @@ public class SellActivity extends AppCompatActivity {
                         String Price = jsonObject.getString("Price");
                         int Quantity = Integer.parseInt(jsonObject.getString("Quantity"));
 
-                        // Kiểm tra xem sản phẩm đã tồn tại trong 'products' chưa
-                        productsRepository.child(ProductCode).addListenerForSingleValueEvent(new ValueEventListener() {
+                        billRepository.child(billCode).child(ProductCode).addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 if (snapshot.exists()) {
-                                    // Sản phẩm đã tồn tại, cập nhật số lượng
-                                    Integer currentQuantity = snapshot.child("Quantity").getValue(Integer.class);
-                                    if (currentQuantity != null) {
-                                        productsRepository.child(ProductCode).child("Quantity").setValue(currentQuantity + Quantity)
-                                                .addOnSuccessListener(aVoid -> {
-                                                    Toast.makeText(SellActivity.this, "Cập nhật số lượng sản phẩm thành công!", Toast.LENGTH_SHORT).show();
-                                                })
-                                                .addOnFailureListener(e -> {
-                                                    Toast.makeText(SellActivity.this, "Cập nhật số lượng sản phẩm thất bại!", Toast.LENGTH_SHORT).show();
-                                                });
+                                    // Kiểm tra xem sản phẩm đã tồn tại trong bill chưa
+                                    Integer currentBillQuantity = snapshot.child("Quantity").getValue(Integer.class);
+                                    if (currentBillQuantity != null) {
+                                        productsRepository.child(ProductCode).child("Quantity").addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot productSnapshot) {
+                                                Integer stockQuantity = productSnapshot.getValue(Integer.class);
+
+                                                if (stockQuantity != null && stockQuantity >= Quantity) {
+                                                    //Cập nhật số lượng nếu đã tồn tại
+                                                    billRepository.child(billCode).child(ProductCode).child("Quantity").setValue(currentBillQuantity + Quantity)
+                                                            .addOnSuccessListener(aVoid -> {
+                                                                Toast.makeText(SellActivity.this, "Cập nhật số lượng sản phẩm thành công!", Toast.LENGTH_SHORT).show();
+
+                                                                //Cập nhật lại kho
+                                                                productsRepository.child(ProductCode).child("Quantity").setValue(stockQuantity - Quantity)
+                                                                        .addOnSuccessListener(aVoid1 -> {
+                                                                            Toast.makeText(SellActivity.this, "Cập nhật kho thành công!", Toast.LENGTH_SHORT).show();
+                                                                        })
+                                                                        .addOnFailureListener(e -> {
+                                                                            Toast.makeText(SellActivity.this, "Cập nhật kho thất bại!", Toast.LENGTH_SHORT).show();
+                                                                        });
+                                                            })
+                                                            .addOnFailureListener(e -> {
+                                                                Toast.makeText(SellActivity.this, "Cập nhật số lượng sản phẩm thất bại!", Toast.LENGTH_SHORT).show();
+                                                            });
+                                                } else {
+                                                    Toast.makeText(SellActivity.this, "Số lượng sản phẩm trong kho không đủ!", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+                                                Toast.makeText(SellActivity.this, "Lỗi khi kiểm tra kho dữ liệu!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
                                     } else {
                                         Toast.makeText(SellActivity.this, "Lỗi khi lấy số lượng hiện tại.", Toast.LENGTH_SHORT).show();
                                     }
                                 } else {
-                                    // Sản phẩm chưa tồn tại, thêm mới
-                                    Product product = new Product(ProductCode, ProductName, Price, Quantity);
-                                    productsRepository.child(ProductCode).setValue(product) //productsRepository
-                                            .addOnSuccessListener(aVoid -> {
-                                                Toast.makeText(SellActivity.this, "Thêm mặt hàng thành công!", Toast.LENGTH_SHORT).show();
-                                            })
-                                            .addOnFailureListener(e -> {
-                                                Toast.makeText(SellActivity.this, "Thêm mặt hàng thất bại!", Toast.LENGTH_SHORT).show();
-                                            });
+                                    //Nếu sản phẩm chưa tồn tại, kiểm tra kho và thêm
+                                    productsRepository.child(ProductCode).child("Quantity").addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot productSnapshot) {
+                                            Integer stockQuantity = productSnapshot.getValue(Integer.class);
+
+                                            if (stockQuantity != null && stockQuantity >= Quantity) {
+                                                Product product = new Product(ProductCode, ProductName, Price, Quantity);
+
+                                                //Thêm sản phẩm vào bill
+                                                billRepository.child(billCode).child(ProductCode).setValue(product)
+                                                        .addOnSuccessListener(aVoid -> {
+                                                            Toast.makeText(SellActivity.this, "Thêm mặt hàng thành công!", Toast.LENGTH_SHORT).show();
+
+                                                            //Cập nhật lại kho
+                                                            productsRepository.child(ProductCode).child("Quantity").setValue(stockQuantity - Quantity)
+                                                                    .addOnSuccessListener(aVoid1 -> {
+                                                                        Toast.makeText(SellActivity.this, "Cập nhật kho thành công!", Toast.LENGTH_SHORT).show();
+                                                                    })
+                                                                    .addOnFailureListener(e -> {
+                                                                        Toast.makeText(SellActivity.this, "Cập nhật kho thất bại!", Toast.LENGTH_SHORT).show();
+                                                                    });
+                                                        })
+                                                        .addOnFailureListener(e -> {
+                                                            Toast.makeText(SellActivity.this, "Thêm mặt hàng thất bại!", Toast.LENGTH_SHORT).show();
+                                                        });
+                                            } else {
+                                                Toast.makeText(SellActivity.this, "Số lượng trong kho không đủ!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+                                            Toast.makeText(SellActivity.this, "Lỗi khi kiểm tra kho dữ liệu!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
                                 }
                             }
 
                             @Override
                             public void onCancelled(@NonNull DatabaseError error) {
-                                Toast.makeText(SellActivity.this, "Lỗi khi thêm dữ liệu.", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SellActivity.this, "Lỗi khi truy cập dữ liệu hóa đơn.", Toast.LENGTH_SHORT).show();
                             }
                         });
                     } catch (JSONException e) {
@@ -157,91 +217,4 @@ public class SellActivity extends AppCompatActivity {
                     }
                 }
             });
-
-    /*//Phương thức tải dữ liệu sản phẩm từ Firebase và cập nhật vào ListView
-    private void loadProductsFromFirebase() {
-        databaseReference.child("bill").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                productList.clear(); // Xóa dữ liệu cũ
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    // Lấy dữ liệu từng sản phẩm
-                    String productCode = dataSnapshot.child("ProductCode").getValue(String.class);
-                    String productName = dataSnapshot.child("ProductName").getValue(String.class);
-                    String price = dataSnapshot.child("Price").getValue(String.class);
-                    Integer quantity = dataSnapshot.child("Quantity").getValue(Integer.class);
-
-                    // Kiểm tra dữ liệu không null
-                    if (productCode != null && productName != null && price != null && quantity != null) {
-                        String productInfo = "Mã sản phẩm: " + productCode + ", Tên: " + productName + ", Giá: " + price + ", Số lượng: " + quantity;
-                        productList.add(productInfo); // Thêm sản phẩm vào danh sách
-                    } else {
-                        Toast.makeText(SellActivity.this, "Dữ liệu không hợp lệ hoặc thiếu thông tin sản phẩm", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                adapter.notifyDataSetChanged(); // Cập nhật ListView
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(SellActivity.this, "Không thể tải dữ liệu.", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    // chạy và xử lý kết quả từ việc quét QR code
-    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
-            result -> {
-                if(result.getContents() == null) {  //Nếu quét bị hủy
-                    Toast.makeText(SellActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
-                } else {
-                    try {
-                        JSONObject jsonObject = new JSONObject(result.getContents());
-                        String ProductCode = jsonObject.getString("Code");
-                        String ProductName = jsonObject.getString("Name");
-                        String Price = jsonObject.getString("Price");
-                        int Quantity = Integer.parseInt(jsonObject.getString("Quantity"));
-
-
-                        Random random = new Random();
-                        int number = random.nextInt(999999) + 1;
-
-                        // Định dạng số để đảm bảo nó có 6 chữ số
-                        String codeBill = String.format("%06d", number);
-
-                        databaseReference.child("bill").addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                if (snapshot.hasChild(codeBill))
-                                    Toast.makeText(SellActivity.this, "Mã đơn đã tồn tại", Toast.LENGTH_SHORT).show();
-                                else{
-                                    Product product = new Product(ProductName, Price, Quantity);
-
-                                    //nếu mã mặt hàng đã tồn tại tăng số lượng sản phẩm
-                                    if (snapshot.hasChild(ProductCode))
-                                    {
-                                        product.Quantity += 1;
-                                    }
-                                    databaseReference.child("bill").child("Code: " + codeBill).child("ProductCode: " +ProductCode).setValue(product).addOnSuccessListener(aVoid -> {
-                                        Toast.makeText(SellActivity.this, "Thêm mặt hàng thành công!", Toast.LENGTH_SHORT).show();
-                                    })
-                                            .addOnFailureListener(e -> {
-                                                Toast.makeText(SellActivity.this, "Thêm mặt hàng thất bại!", Toast.LENGTH_SHORT).show();
-                                            });
-                                    // Lấy dữ liệu từ Firebase và cập nhật vào ListView
-                                    loadProductsFromFirebase();
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-                                Toast.makeText(SellActivity.this, "Lỗi khi thêm dữ liệu.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Toast.makeText(SellActivity.this, "Dữ liệu QR không hợp lệ.", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });*/
 }
